e0da4daf09a225362988247541b5748e57fc3745
Max <mmyt96@student.cis.edu.hk>
2021-04-22 22:53:05 +0800

    workin on it, the edit habit activity shows the habit's data now, but you can't update it just yet

--- HabitApp/app/src/main/java/com/example/habitapp/EditHabitActivity.java
+++ HabitApp/app/src/main/java/com/example/habitapp/EditHabitActivity.java
@@ -4,11 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.example.habitapp.enums.Goal;
 import com.example.habitapp.models.Habit;
 import com.example.habitapp.utils.HabitConstants;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,6 +18,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
 
 public class EditHabitActivity extends AppCompatActivity {
 
@@ -33,6 +37,16 @@ public class EditHabitActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_habit);
 
+        editDateInput = findViewById(R.id.editDateInput);
+        deleteGoalButton = findViewById(R.id.deleteGoalButton);
+        deleteHabitButton = findViewById(R.id.deleteHabitButton);
+        deleteDateButton = findViewById(R.id.deleteDateButton);
+        goalSpinner = findViewById(R.id.goalSpinner);
+        editTitleInput = findViewById(R.id.editTitleInput);
+        editGoalInput = findViewById(R.id.editGoalInput);
+        editTitle = findViewById(R.id.editTitle);
+
+
         mAuth = FirebaseAuth.getInstance();
     }
 
@@ -46,13 +60,32 @@ public class EditHabitActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                 if (task.isSuccessful()) {
                     habit = task.getResult().toObject(Habit.class);
+                    updateUI();
                 }
             }
 
         });
     }
 
+    private void updateUI() {
+        editTitleInput.setText(habit.getTitle());
 
+        if (habit.getGoalType() == Goal.NONE) {
+            editGoalInput.setVisibility(View.INVISIBLE);
+        } else {
+            deleteGoalButton.setVisibility(View.VISIBLE);
+            editGoalInput.setText(habit.getGoal() + "");
+            System.out.println("peepeepoopoo " + habit.getGoal());
+        }
+
+        if (habit.getGoalDate() == null) {
+            editDateInput.setVisibility(View.INVISIBLE);
+        } else {
+            deleteDateButton.setVisibility(View.VISIBLE);
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            editDateInput.setText(formatter.format(habit.getGoalDate()));
+        }
+    }
 
 
 }
