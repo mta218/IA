475cfa457106dfabd0307718f2672add9bcf0a5a
Max <mmyt96@student.cis.edu.hk>
2021-06-03 12:51:42 +0800

    dark mode yesh

--- HabitApp/app/src/main/java/com/example/habitapp/SettingsActivity.java
+++ HabitApp/app/src/main/java/com/example/habitapp/SettingsActivity.java
@@ -49,46 +49,15 @@ public class SettingsActivity extends AppCompatActivity {
         fRef = FirebaseFirestore.getInstance();
         getUserInfo();
 
-        // Saving state of our app
-        // using SharedPreferences
-        //SharedPreferences sharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
-        //final SharedPreferences.Editor editor = sharedPreferences.edit();
-        //final boolean isDarkModeOn = sharedPreferences.getBoolean("isDarkModeOn", false);
-
-        // When user reopens the app
-        // after applying dark/light mode
-        //if (isDarkModeOn) {
-        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-        //} else {
-        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        //}
-
         darkModeSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // When user taps the enable/disable
-                // dark mode button
                 if (!darkModeSwitch.isChecked()) {
-
-                    // if dark mode is on it
-                    // will turn it off
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                    // it will set isDarkModeOn
-                    // boolean to false
-                    //editor.putBoolean("isDarkModeOn", false);
-                    //editor.apply();
                     user.getSettings().setDarkMode(false);
 
                 } else {
-                    // if dark mode is off
-                    // it will turn it on
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-
-                    // it will set isDarkModeOn
-                    // boolean to true
-                    //editor.putBoolean("isDarkModeOn", true);
-                    //editor.apply();
-
                     user.getSettings().setDarkMode(true);
 
                 }
